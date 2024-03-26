4290c78ec139cc4eb8062087e2babb5e50e88e71
divyamprabhudessai <divyam.desai@kalvium.community>
2024-03-26 12:11:44 +0530

    added default port

--- Server/server.js
+++ Server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 require('dotenv').config();
 const router = require('./routes.js');
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 3000
 const {startDb} = require('./db.js')
 
 app.use(router);
